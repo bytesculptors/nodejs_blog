7cadbdb2addf259652b948b16ae638522cdff096
Tuan Nghia <tuannghia20062003@gmmail.com>
2023-06-24 11:13:14 +0700

    Add img file

--- src/index.js
+++ src/index.js
@@ -9,6 +9,8 @@ const port = 3000;
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
+app.use(express.static(path.join(__dirname, 'public')));
+
 app.engine('hbs', engine({
     extname: '.hbs'
 }));
